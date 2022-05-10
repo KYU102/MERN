e3e8d7d4905721ad0d1634fcb2c19bbb2b801b90
KYU102 <kevinyu25@gmail.com>
2022-05-10 12:41:12 -0700

    update: added button to display info instead

--- react/axios_pokemon_api/src/Components/Display.jsx
+++ react/axios_pokemon_api/src/Components/Display.jsx
@@ -4,26 +4,40 @@ import axios from 'axios'
 const MainUseEffect = (props) => {
     const [apiState, setAPIState] = useState()
 
-    useEffect(() => {
+    const fetchPokemon = () => {
         axios.get("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0") // MAKING A REQUEST TO GITHUB API
             .then(successResponse => {
                 console.log("SUCCESS: ", successResponse.data)
                 setAPIState(successResponse.data.results) // Taking the response data and storing it in state
             }) // successful response
             .catch(errorResponse => console.log("ERROR: ", errorResponse)) // unsuccessful response
-    }, [])
+    }
 
     return (
         <fieldset>
-            <legend>MainUseEffect.jsx</legend>
+            <legend>FetchPokemon.jsx</legend>
+            <button onClick={fetchPokemon}>Fetch Pokemon</button>
+            {/* hotwire to display */}
             {
-                apiState &&
+
+                // apiState &&
                 
-                apiState.map(poke => {
+                // apiState.map(poke => {
+                //     return(
+                //         <h1>{poke.name}</h1>
+                //     )
+                // })
+
+
+                // useing a turnary to display
+                (apiState) ?
+                <>
+                {apiState.map(poke => {
                     return(
                         <h1>{poke.name}</h1>
                     )
-                })
+                })}
+                </> : <h1>Loading.....</h1>
             }
         </fieldset>
     )
