e3962fa96d05b2d997381afc0c150e028f748743
KYU102 <kevinyu25@gmail.com>
2022-05-11 18:42:12 -0700

    update: added error msg if person doesnt exist

--- react/luke_apiwalker/src/components/people.jsx
+++ react/luke_apiwalker/src/components/people.jsx
@@ -5,65 +5,63 @@ import { useParams } from 'react-router';
 const People = () => {
     const { id } = useParams()
     const [apiState, setAPIState] = useState()
+    const [err, setErr] = useState(null)
+
 
     useEffect(() => {
         axios.get(`https://swapi.dev/api/people/${id}`) // MAKING A REQUEST TO GITHUB API
             .then(successResponse => {
                 console.log("success" + successResponse.data)
                 setAPIState(successResponse.data)
+                setErr(null)
             })                  // Taking the response data and storing it in state
-            .catch (errorResponse => console.log("ERROR: ", errorResponse))
+            .catch(err => {
+                setAPIState("")
+                setErr("these are not the droids you are looking for")
+            })
 
     }, [id])
 
-
-
-
+    const showPeople = () => {
         return (
             <fieldset>
                 <legend>FetchPeople.jsx</legend>
                 {
-                apiState ? 
-                    <div> 
-                        <h1> Name: {apiState.name}</h1> 
-                        <h2> Height: {apiState.height}</h2> 
-                        <h2> Mass: {apiState.mass}</h2> 
-                        <h2> Skin Color: {apiState.skin_color}</h2> 
-                        <h2> Hair Color: {apiState.hair_color}</h2> 
-                    
-                    </div> 
-                : <h1> Loading...</h1>
-                }   
-                {/* hotwire to display */}
-                {
+                    apiState ?
+                        <div>
+                            <h1> Name: {apiState.name}</h1>
+                            <h2> Height: {apiState.height}</h2>
+                            <h2> Mass: {apiState.mass}</h2>
+                            <h2> Skin Color: {apiState.skin_color}</h2>
+                            <h2> Hair Color: {apiState.hair_color}</h2>
 
-                    // apiState &&
-
-                    // apiState.map(poke => {
-                    //     return(
-                    //         <h1>{poke.name}</h1>
-                    //     )
-                    // })
-
-
-                    // useing a turnary to display
-                    // (apiState) ?
-                    //     <>
-                    //         {apiState.map((person, index) => {
-                    //             return (
-                    //                 <div key={index}>
-                    //                     <h1>{person.name} {index}</h1>
-                    //                     <p>Height: {person.height}</p>
-                    //                     <p>Mass: {person.mass}</p>
-                    //                     <p>Hair Color: {person.hair_color}</p>
-                    //                     <p>Skin Color: {person.skin_color}</p>
-                    //                 </div>
-                    //             )
-                    //         })}
-                    //     </> : null
+                        </div>
+                        : <h1> Loading...</h1>
                 }
+
             </fieldset>
         )
     }
+
+    const showError = () => {
+        return (
+            <div>
+            <p>{err}</p>
+            <img src='https://i.ytimg.com/vi/pUZo2Do2mcY/maxresdefault.jpg'/>
+            </div>
+        )
+    }
+
+
+    return (
+        <fieldset>
+            <legend>FetchPeople.jsx</legend>
+            {
+                err ?
+                    showError() : showPeople()
+            }
+        </fieldset>
+    )
+}
 
 export default People
